e957e3e7315da726a897eacd2048311f3375a02e
Kirill <macgenr0@gmail.com>
2022-09-12 15:57:16 +0300

    adaptive header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+'use strict';
+
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+	menu.classList.add('menu_active');
+});
+
+closeElem.addEventListener('click', (e) => {
+	menu.classList.remove('menu_active');
+})
